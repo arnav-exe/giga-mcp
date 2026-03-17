28ed9e51556acfe4aec7e6b37efad6dc7fb68772
arnav-exe <arnav.jagetia03@gmail.com>
2026-03-17 09:32:14 -0400

    added pytest scaffolding

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+FIXTURES_DIR = Path(__file__).parent / "fixtures"
+DISCOVERY_FIXTURES_DIR = FIXTURES_DIR / "discovery"
+RETRIEVAL_FIXTURES_DIR = FIXTURES_DIR / "retrieval"
